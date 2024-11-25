fdccd837710d9e7d7c5c332b86aa9dc56cdba0d2
Sumit Kumar <kumarsumitjat298@gmail.com>
2024-11-25 13:09:40 +0530

    Implemented basic methods to support servicer interface

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,29 @@
 package service
+
+import (
+	"auth-rest-api/internal/models"
+	"context"
+)
+
+type Storer interface {
+}
+
+type Service struct {
+	Store Storer
+}
+
+func New(s Storer) *Service {
+	return &Service{Store: s}
+}
+
+func (s *Service) SignIn(ctx context.Context, user *models.UserReq) (string, error) {
+	return "", nil
+}
+
+func (s *Service) SignUp(ctx context.Context, user *models.UserReq) (string, error) {
+	return "", nil
+}
+
+func (s *Service) RefreshToken(ctx context.Context, user *models.UserReq) (string, error) {
+	return "", nil
+}
