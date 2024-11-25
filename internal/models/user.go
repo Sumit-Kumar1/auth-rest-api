1f5adf43341e099ba56bf80c226df0afb34aa51d
Sumit Kumar <kumarsumitjat298@gmail.com>
2024-11-25 13:21:00 +0530

    Added user req validation method

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"errors"
+	"regexp"
+	"strings"
+)
+
 type UserReq struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
@@ -8,4 +14,29 @@ type UserReq struct {
 type UserResp struct {
 	Email string `json:"email"`
 	Token string `json:"token"`
+}
+
+func (u *UserReq) Validate() error {
+	email := strings.ToLower(strings.TrimSpace(u.Email))
+	passwd := strings.TrimSpace(u.Password)
+
+	emailRegex := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+
+	if email == "" {
+		return errors.New("email is required")
+	}
+
+	if !emailRegex.MatchString(email) {
+		return errors.New("invalid email")
+	}
+
+	if passwd == "" {
+		return errors.New("password is required")
+	}
+
+	if len(passwd) < 8 {
+		return errors.New("password is too short")
+	}
+
+	return nil
 }
