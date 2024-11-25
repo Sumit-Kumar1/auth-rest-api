dc394213f10058b065b6828efe8dfba9d642998e
Sumit Kumar <kumarsumitjat298@gmail.com>
2024-11-25 13:12:24 +0530

    Added layer init and route assignment with handler func, added basic health endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log/slog"
+	"net/http"
 	"os"
 	"os/signal"
 
+	"auth-rest-api/internal/handler"
 	"auth-rest-api/internal/server"
+	"auth-rest-api/internal/service"
+	"auth-rest-api/internal/store"
 )
 
 func main() {
@@ -18,6 +23,8 @@ func main() {
 		app.Logger.LogAttrs(ctx, slog.LevelError, "failed to create server", slog.Any("error", err))
 		return
 	}
+
+	newHTTPHandler(app)
 
 	srvErr := make(chan error, 1)
 	go func() {
@@ -44,4 +51,53 @@ func main() {
 	}
 
 	app.Logger.LogAttrs(ctx, slog.LevelInfo, "application is shut down", slog.String("name", app.Name))
+}
+
+func newHTTPHandler(app *server.Server) {
+	st := store.New(app.DB)
+	svc := service.New(st)
+	h := handler.New(svc)
+
+	app.Mux.HandleFunc("POST /signin", server.Chain(h.SignIn, server.AddCorrelation()))
+	app.Mux.HandleFunc("POST /signup", server.Chain(h.SignUp, server.AddCorrelation()))
+	app.Mux.HandleFunc("POST /refresh", server.Chain(h.RefreshToken, server.AddCorrelation()))
+
+	app.Mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		if err := app.DB.Client.Ping(context.Background()); err != nil {
+			app.Health = &server.Health{
+				Status:   "Down",
+				DBStatus: "Down",
+			}
+
+			data, mErr := json.Marshal(app.Health)
+			if mErr != nil {
+				http.Error(w, "not able to marshal the health status", http.StatusInternalServerError)
+				return
+			}
+
+			app.Logger.LogAttrs(ctx, slog.LevelDebug, "health status", slog.Any("status", app.Health))
+
+			w.Header().Set("Content-Type", "application/json")
+			_, _ = w.Write(data)
+			return
+		}
+
+		app.Health = &server.Health{
+			Status:   "Up",
+			DBStatus: "Up",
+		}
+
+		app.Logger.LogAttrs(ctx, slog.LevelDebug, "health status", slog.Any("status", app.Health))
+
+		data, mErr := json.Marshal(app.Health)
+		if mErr != nil {
+			http.Error(w, "not able to marshal the health status", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = w.Write(data)
+	})
 }
