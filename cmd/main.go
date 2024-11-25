535bc3e6bed61876eb79de7e5c286e6c44542496
Sumit Kumar <kumarsumitjat298@gmail.com>
2024-11-25 13:11:19 +0530

    created a simple custom golang server with graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+
+	"auth-rest-api/internal/server"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	app, err := server.ServerFromEnvs()
+	if err != nil {
+		app.Logger.LogAttrs(ctx, slog.LevelError, "failed to create server", slog.Any("error", err))
+		return
+	}
+
+	srvErr := make(chan error, 1)
+	go func() {
+		app.Logger.LogAttrs(ctx, slog.LevelInfo, "application is running",
+			slog.Group("server", slog.String("name", app.Name), slog.String("address", app.Addr),
+				slog.Bool("DB Connected", true), slog.Group("timeouts (durations)", slog.Duration("read", app.ReadTimeout),
+					slog.Duration("write", app.WriteTimeout), slog.Duration("idle", app.IdleTimeout))))
+
+		app.Server.Handler = app.Mux
+		srvErr <- app.ListenAndServe()
+	}()
+
+	select {
+	case err = <-srvErr:
+		app.Logger.Error(err.Error())
+		return
+	case <-ctx.Done():
+		stop()
+	}
+
+	err = app.Shutdown(context.Background())
+	if err != nil {
+		app.Logger.LogAttrs(ctx, slog.LevelError, "error while shutting down", slog.String("error", err.Error()))
+	}
+
+	app.Logger.LogAttrs(ctx, slog.LevelInfo, "application is shut down", slog.String("name", app.Name))
 }
