6733075910b1e74182b212d47c95e76ed44b21d8
Sumit Kumar <kumarsumitjat298@gmail.com>
2024-11-25 20:43:39 +0530

    Added signIn logic of token generation

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,8 +1,12 @@
 package service
 
 import (
-	"auth-rest-api/internal/models"
 	"context"
+	"errors"
+	"log/slog"
+
+	"auth-rest-api/internal/models"
+	"auth-rest-api/internal/server"
 )
 
 type Storer interface {
@@ -17,7 +21,23 @@ func New(s Storer) *Service {
 }
 
 func (s *Service) SignIn(ctx context.Context, user *models.UserReq) (string, error) {
-	return "", nil
+	logger := ctx.Value(server.Logger).(*slog.Logger)
+
+	if user == nil {
+		logger.LogAttrs(ctx, slog.LevelError, "empty user struct provided")
+		return "", errors.New("empty user strut")
+	}
+
+	if err := user.Validate(); err != nil {
+		return "", err
+	}
+
+	token, err := GenerateToken(user.Email)
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
 }
 
 func (s *Service) SignUp(ctx context.Context, user *models.UserReq) (string, error) {
