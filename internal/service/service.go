fd19b1f70551403fe7c40f351009aefc5aee6b1c
Sumit Kumar <kumarsumitjat298@gmail.com>
2024-11-27 15:42:38 +0530

    Completed logic for token refresh and revoke

--- internal/service/service.go
+++ internal/service/service.go
@@ -7,9 +7,15 @@ import (
 
 	"auth-rest-api/internal/models"
 	"auth-rest-api/internal/server"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Storer interface {
+	CreateUser(ctx context.Context, u *models.UserData) error
+	GetUserByEmail(ctx context.Context, email string) (*models.UserData, error)
+	CreateToken(ctx context.Context, email string, td *models.TokenData) error
+	DeleteToken(ctx context.Context, tokenID ...string) error
 }
 
 type Service struct {
@@ -20,30 +26,124 @@ func New(s Storer) *Service {
 	return &Service{Store: s}
 }
 
-func (s *Service) SignIn(ctx context.Context, user *models.UserReq) (string, error) {
+func (s *Service) SignUp(ctx context.Context, user *models.UserReq) error {
 	logger := ctx.Value(server.Logger).(*slog.Logger)
 
 	if user == nil {
 		logger.LogAttrs(ctx, slog.LevelError, "empty user struct provided")
-		return "", errors.New("empty user strut")
+		return errors.New("empty user strut")
 	}
 
 	if err := user.Validate(); err != nil {
-		return "", err
+		return err
 	}
 
-	token, err := GenerateToken(user.Email)
+	exUser, err := s.Store.GetUserByEmail(ctx, user.Email)
+	if err != nil && !errors.Is(models.ErrNotFound("user"), err) {
+		return err
+	}
+
+	if exUser != nil {
+		return models.ErrUserAlreadyExists
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
 	if err != nil {
-		return "", err
+		return err
 	}
 
-	return token, nil
+	ud := models.UserData{
+		Email:    user.Email,
+		Password: hash,
+	}
+
+	if err := s.Store.CreateUser(ctx, &ud); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func (s *Service) SignUp(ctx context.Context, user *models.UserReq) (string, error) {
-	return "", nil
+func (s *Service) SignIn(ctx context.Context, user *models.UserReq) (access, refresh string, err error) {
+	logger := ctx.Value(server.Logger).(*slog.Logger)
+
+	if user == nil {
+		logger.LogAttrs(ctx, slog.LevelError, "empty user struct provided")
+		return "", "", models.ErrInvalid("user")
+	}
+
+	if valErr := user.Validate(); valErr != nil {
+		return "", "", valErr
+	}
+
+	exUser, err := s.Store.GetUserByEmail(ctx, user.Email)
+	if err != nil {
+		return "", "", err
+	}
+
+	if err = bcrypt.CompareHashAndPassword(exUser.Password, []byte(user.Password)); err != nil {
+		logger.LogAttrs(ctx, slog.LevelError, "wrong password")
+		return "", "", models.ErrPsswdNotMatch
+	}
+
+	tokenData, err := GenerateToken(user.Email)
+	if err != nil {
+		return "", "", err
+	}
+
+	if err := s.Store.CreateToken(ctx, user.Email, tokenData); err != nil {
+		return "", "", err
+	}
+
+	return tokenData.AccessToken, tokenData.RefreshToken, nil
 }
 
-func (s *Service) RefreshToken(ctx context.Context, user *models.UserReq) (string, error) {
-	return "", nil
+func (s *Service) RefreshToken(ctx context.Context, accessToken, refreshToken string) (access, refresh string, err error) {
+	logger := ctx.Value(server.Logger).(*slog.Logger)
+
+	accClaims, err := ParseToken(accessToken, "access")
+	if err != nil {
+		logger.LogAttrs(ctx, slog.LevelError, "invalid access token", slog.String("token", accessToken), slog.String("error", err.Error()))
+		return "", "", err
+	}
+
+	refClaims, err := ParseToken(refreshToken, "refresh")
+	if err != nil {
+		logger.LogAttrs(ctx, slog.LevelError, "invalid refresh token", slog.String("token", refreshToken), slog.String("error", err.Error()))
+		return "", "", err
+	}
+
+	// Deleting old active tokens
+	if delErr := s.Store.DeleteToken(ctx, accClaims.ClaimUID, refClaims.ClaimUID); delErr != nil {
+		return "", "", delErr
+	}
+
+	// generate new tokens for that email
+	td, err := GenerateToken(accClaims.Email)
+	if err != nil {
+		return "", "", err
+	}
+
+	// store the newly generated tokens UIDs
+	if err := s.Store.CreateToken(ctx, accClaims.Email, td); err != nil {
+		return "", "", err
+	}
+
+	return td.AccessToken, td.RefreshToken, nil
+}
+
+func (s *Service) RevokeToken(ctx context.Context, token string) error {
+	logger := ctx.Value(server.Logger).(*slog.Logger)
+
+	accClaims, err := ParseToken(token, "access")
+	if err != nil {
+		logger.LogAttrs(ctx, slog.LevelError, "invalid access token", slog.String("token", token), slog.String("error", err.Error()))
+		return err
+	}
+
+	if delErr := s.Store.DeleteToken(ctx, accClaims.ClaimUID); delErr != nil {
+		return delErr
+	}
+
+	return nil
 }
